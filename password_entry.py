d9502a0fbc21b83cb3bc5331400e3cfa31b6d2aa
Matthew Nielsen <mnielsen10521@gmail.com>
2019-08-22 11:54:30 +1000

    Add * to print on same line

--- password_entry.py
+++ password_entry.py
@@ -5,5 +5,5 @@ password_length = len(password)
 while password_length < min_length:
     print("Error password must be at least 5 characters long")
     password = input("Enter Password: ")
-for letter in range(0, password):
-    print("*")
+for letter in range(0, password_length):
+    print("*", end="")
