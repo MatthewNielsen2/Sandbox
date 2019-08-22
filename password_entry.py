47da3cf29a7f9997dcc6a8e4f49ac40af1cae4b6
Matthew Nielsen <mnielsen10521@gmail.com>
2019-08-22 11:51:52 +1000

    Add loop with password length checker
    Add for loop for characters in password

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 """Matthew Nielsen"""
+min_length = 5
+password = input("Enter Password: ")
+password_length = len(password)
+while password_length < min_length:
+    print("Error password must be at least 5 characters long")
+    password = input("Enter Password: ")
+for letter in range(0, password):
+    print("*")
